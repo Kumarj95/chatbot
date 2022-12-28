cc9e4d3f11f2f6df135565a1addf221bdb919657
Kumar Vaibhav Jha <jha.kumarv@gmail.com>
2022-12-27 22:07:44 -0500

    Made some changes to openai api key

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import openai
 
 load_dotenv()
-openai.api_key = os.environ.get('OPENAI_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 completion = openai.Completion()
 
 start_chat_log = '''Human: Hello, who are you?
